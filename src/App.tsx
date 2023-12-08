ed4a2f996fd2e7c5c699fa954b95c39022010a2b
5150 <5150@tower.corp>
2023-12-08 16:51:49 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import { ReactElement, useState } from 'react'
+import { NavLink, Route, Routes } from 'react-router-dom'
+import { List } from './components/List'
+import './App.css'
+
+const Header = () : ReactElement => {
+  return (
+    <div className='header'>
+      Recipes Heaven
+      {' '}
+      <NavLink to="/" >Список</NavLink>
+      {' | '}
+      <NavLink to="/recipes/new">Добавить</NavLink>
+    </div>
+  )
+}
+
+// const List = () : ReactElement => {
+//   return (
+//     <ul className='list'>
+//       <li>Торт Наполеон</li>
+//       <li>Борщ</li>
+//       <li>Пельмени</li>
+//       <li>Картофельное пюре</li>
+//     </ul>
+//   )
+// }
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <div className='content'>
+        <Routes>
+          <Route path="/" element={<List/>}></Route>
+        </Routes>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/List.tsx
@@ -0,0 +1,35 @@
+import {Link} from "react-router-dom";
+
+const RECIPES = {
+    '1': {
+        title: 'Торт Наполеон',
+    },
+    '2': {
+        title: 'Борщ',
+    },
+    '3': {
+        title: 'Пельмени',
+    },
+    '4': {
+        title: 'Картофельное пюре',
+    },
+}
+
+const List = () : ReactElement => {
+    return (
+        <>
+            <ul>
+                {Object
+                    .entries(RECIPES)
+                    .map(([rId, recipe], idx) => (
+                        <li key={rId}>
+                            <Link to={`/recipes/${rId}`}>
+                                {recipe.title}
+                            </Link>
+                        </li>
+                    ))
+                }
+            </ul>
+        </>
+    )
+}
